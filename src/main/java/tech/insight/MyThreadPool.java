1edd5b393b744b4492d6d7bee1df118c909e3fd9
Myrxmy <1655895179@qq.com>
2025-09-21 16:39:31 +0800

    拥有一个线程的阻塞队列线程池

--- src/main/java/tech/insight/MyThreadPool.java
+++ src/main/java/tech/insight/MyThreadPool.java
@@ -1,8 +1,27 @@
 package main.java.tech.insight;
 
-public class MyThreadPool {
-    void execute(Runnable command) {
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class MyThreadPool {
+
+    BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<>(1024);
+
+    Thread thread = new Thread(() ->{
+        while (true) {
+            try {
+                Runnable command = blockingQueue.take();
+                command.run();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    });
+
+
+    void execute(Runnable command) {
+        boolean offer = blockingQueue.offer(command);
     }
 
 }
